971305816f3d379328b2646b86505666ab8fda65
mauro arias <mauro.arias.a@gmail.com>
2022-03-22 22:01:34 -0400

    testing

--- vars/pipelineCi.groovy
+++ vars/pipelineCi.groovy
@@ -127,7 +127,7 @@ def call(body) {
                                 agent {
                                     docker {
                                         image "${agentName}"
-                                        args '−−net=host'
+                                        args '--network host'
                                     }
                                 }                
                                 steps {
